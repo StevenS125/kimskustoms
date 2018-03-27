76dd358ce0d64ca90581094776f2d9368519b385
StevenS125 <sts12589@gmail.com>
2018-03-27 13:11:28 -0400

    story completed

--- src/Story/Story.js
+++ src/Story/Story.js
@@ -5,15 +5,16 @@ export default class Story extends React.Component {
   render() {
     return (
         <div className={styles.container}>
-          <div className={styles.landingleft}>
-          <h1>Welcome  To  KIM'S  KUSTOM'S  Furniture</h1>
+          <div className={styles.storyleft}>
+          <h1>MEET KIM OF KIM'S KUSTOMS</h1>
           
-          <p>Do you have furniture you'd like to be restored?
-            Here at Kim's Kustoms we may not know how to spell 
-            but we sure do love fixing up antique furniture at an excellent price and value to you
+          <p>Kim found her passion of restoring furniture in 2010 and 
+              decided to open her own small business shortly after. Along with being 
+              an artist with her furniture she is a wife and mother to a beautiful daughter.
           </p>
           </div>
-            <div className={styles.landingright}>
+            <div className={styles.storyright}>
+            <img src="../../becca.jpeg" alt="Kim the owner" className={styles.picture}/>
             </div>
 
       </div>
